b30868bbb00349a465b6f19e1bd57b90418b8801
seeseekey <kontakt@seeseekey.net>
2015-07-29 16:34:52 +0200

    Implement stop timer class.

---
+++ Various/StopTimer.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace Xevle.Core
+{
+	public class StopTimer
+	{
+		#region Private variables
+		Int64 startTime;
+		Int64 stopTime;
+
+		StopTimerState timerState;
+		#endregion
+
+		#region Constructor
+		public StopTimer()
+		{
+			startTime = 0;
+			stopTime = 0;
+			
+			timerState = StopTimerState.Stop;
+		}
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Returns the correct stop timer difference.
+		/// </summary>
+		/// <value>The difference.</value>
+		Int64 Difference
+		{
+			get
+			{
+				switch (timerState)
+				{
+					case StopTimerState.Start:
+						{
+							return DateTime.Now.Ticks - startTime;
+						}
+					case StopTimerState.Stop:
+						{
+							return stopTime - startTime;
+						}
+					default:
+						{
+							return 0;
+						}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in micro seconds.
+		/// </summary>
+		/// <value>The micro seconds.</value>
+		public double MicroSeconds
+		{
+			get
+			{
+				return Difference / (10.0);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in micro seconds.
+		/// </summary>
+		/// <value>The milli seconds.</value>
+		public double MilliSeconds
+		{
+			get
+			{
+				return Difference / (10000.0);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in seconds.
+		/// </summary>
+		/// <value>The seconds.</value>
+		public double Seconds
+		{
+			get
+			{
+				return Difference / (10000000.0);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in minutes.
+		/// </summary>
+		/// <value>The minutes.</value>
+		public double Minutes
+		{
+			get
+			{
+				return Difference / (10000000.0 * 60);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in hours.
+		/// </summary>
+		/// <value>The hours.</value>
+		public double Hours
+		{
+			get
+			{
+				return Difference / (10000000.0 * 60 * 60);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in days.
+		/// </summary>
+		/// <value>The days.</value>
+		public double Days
+		{
+			get
+			{
+				return (10000000.0 * 60 * 60 * 24);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in weeks.
+		/// </summary>
+		/// <value>The weeks.</value>
+		public double Weeks
+		{
+			get
+			{
+				return Difference / (10000000.0 * 60 * 60 * 24 * 7);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in months.
+		/// </summary>
+		/// <value>The months.</value>
+		public double Months
+		{
+			get
+			{
+				return Difference / (10000000.0 * 60 * 60 * 24 * 30.44);
+			}
+		}
+
+		/// <summary>
+		/// Returns the stopwatch time in years.
+		/// </summary>
+		/// <value>The years.</value>
+		public double Years
+		{
+			get
+			{
+				return Difference / (10000000.0 * 60 * 60 * 24 * 365.25);
+			}
+		}
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Start the stop watch.
+		/// Previos state would be cleared.
+		/// </summary>
+		public void Start()
+		{
+			if (timerState == StopTimerState.Start)
+			{
+				throw new Exception("Timer is already started!");
+			}
+
+			startTime = DateTime.Now.Ticks;
+			timerState = StopTimerState.Start;
+		}
+
+		/// <summary>
+		/// Stop this stop watch.
+		/// </summary>
+		public void Stop()
+		{
+			stopTime = DateTime.Now.Ticks;
+			timerState = StopTimerState.Stop;
+		}
+		#endregion
+	}
+}
+
---
+++ Various/StopTimerState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xevle.Core
+{
+	/// <summary>
+	/// Stop timer state.
+	/// </summary>
+	public enum StopTimerState
+	{
+		Stop,
+		Start,
+		Pause,
+		Resume
+	}
+}
+
