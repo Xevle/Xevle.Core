79883e5df84b0dfed20ddae16269f5d3a5e50cd6
seeseekey <kontakt@seeseekey.net>
2015-08-05 19:37:38 +0200

    Add IListExtensions.

---
+++ Extensions/IListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xevle.Core
+{
+	public static class IListExtensions
+	{
+		public static void Shuffle<T>(this IList<T> list)
+		{
+			Random random=new Random();
+			int n=list.Count;
+
+			while(n>1)
+			{
+				n--;
+				int k=random.Next(n+1);
+
+				T value=list[k];
+				list[k]=list[n];
+				list[n]=value;
+			}
+		}
+	}
+}
+
